ce06ac971596070d7874882a582bebf768c78b86
Said El Atmioui <said.elatmioui@gmail.com>
2024-09-03 11:25:00 +0200

    Add default item update strategy

---
+++ src/main/java/com/gildedrose/DefaultStrategy.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+public class DefaultStrategy implements UpdateStrategy {
+
+    @Override
+    public void update(Item item) {
+        decreaseQuality(item);
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            decreaseQuality(item);
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+    }
+}
