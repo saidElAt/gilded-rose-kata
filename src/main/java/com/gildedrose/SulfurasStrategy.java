198d10fd2e31a33a985c8be59a6cfbd1e0a6352c
Said El Atmioui <said.elatmioui@gmail.com>
2024-09-09 09:30:00 +0200

    Add Sulfuras update strategy

---
+++ src/main/java/com/gildedrose/SulfurasStrategy.java
@@ -0,0 +1,9 @@
+package com.gildedrose;
+
+public class SulfurasStrategy implements UpdateStrategy {
+
+    @Override
+    public void update(Item item) {
+        // Sulfuras doesn't change
+    }
+}
